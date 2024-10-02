ba426cd7218a106296b592727f613aa83e944a86
Khalil <Khalilhashemi.h@gmail.com>
2024-10-02 13:09:24 +0330

    add to footer social icons

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,7 +3,12 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import BookIcon from "@mui/icons-material/Book";
 import Container from "@mui/material/Container";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Button } from "@mui/material";
 
 function Layout({ children }) {
   const navigate = useNavigate();
@@ -40,9 +45,53 @@ function Layout({ children }) {
           color="primary"
           padding="10px"
           textAlign="center"
+          fontWeight={700}
+          fontSize={25}
           mt={10}
+          py={3}
         >
-          پروژه وبلاگ | خلیل هاشمی
+          پروژه وبلاگ
+        </Typography>
+        <Typography
+          component="p"
+          variant="p"
+          textAlign="center"
+          bgcolor="#f7f7f7"
+          pb={5}
+        >
+          <Link to="https://t.me/KhalilHashemi8" target="_blank">
+            <Button sx={{ borderRadius: "10px" }}>
+              <InstagramIcon sx={{ fontSize: "30px", paddingY: "8px" }} />
+            </Button>
+          </Link>
+          <Link to="https://telegram.org/khalilhashemi8" target="_blank">
+            <Button sx={{ borderRadius: "10px" }}>
+              <TelegramIcon sx={{ fontSize: "30px", paddingY: "8px" }} />
+            </Button>
+          </Link>
+          <Link to="https://github.com/KhalilHashemi" target="_blank">
+            <Button sx={{ borderRadius: "10px" }}>
+              <GitHubIcon sx={{ fontSize: "30px", paddingY: "8px" }} />
+            </Button>
+          </Link>
+          <Link
+            to="https://www.linkedin.com/in/khalil-hashemi-049b6b219"
+            target="_blank"
+          >
+            <Button sx={{ borderRadius: "10px" }}>
+              <LinkedInIcon sx={{ fontSize: "30px", paddingY: "8px" }} />
+            </Button>
+          </Link>
+        </Typography>
+        <Typography
+          component="p"
+          variant="p"
+          textAlign="center"
+          bgcolor="#f7f7f7"
+          pb={5}
+          color="secondary"
+        >
+          خلیل هاشمی | © 2024 All rights reserved
         </Typography>
       </footer>
       {/*----------------------------- </footer> ---------------------------*/}
